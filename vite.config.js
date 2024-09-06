019097e0b458bd8b6d6778d58478345320862ae9
Gehad Salem Abd El-shafy <35219807+GehadSalem@users.noreply.github.com>
2024-09-06 19:22:16 +0300

    edit

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     proxy:{
       '/api':{
         target: 'https://real-state-api-1dpy.vercel.app/',
-        secure: true,
+        secure: false,
       },
     },
   },
