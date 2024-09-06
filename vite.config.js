df0a2293a0ad73f0ebb5754e51f9326ec88fe6b5
Gehad Salem Abd El-shafy <35219807+GehadSalem@users.noreply.github.com>
2024-09-06 19:48:20 +0300

    edit

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,11 @@ import react from '@vitejs/plugin-react-swc';
 export default defineConfig({
   server: {
     proxy: {
-      '/': {
+      '/api': {
         target: 'https://real-state-api-1dpy.vercel.app',
+        changeOrigin: true,
         secure: false,
+        rewrite: (path) => path.replace(/^\/api/, ''), // تحقق من أن هذا يتطابق مع المسارات في الـ Backend
       },
     },
   },
