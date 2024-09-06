fb380b1d33f14b2247ed84f3c4a20782d1820802
Gehad Salem Abd El-shafy <35219807+GehadSalem@users.noreply.github.com>
2024-09-06 22:05:45 +0300

    edit

--- src/firebase.js
+++ src/firebase.js
@@ -6,7 +6,7 @@ import { initializeApp } from "firebase/app";
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyBMEA9_mJV155UQLVOTvW9l959KVJK5bug",
-  authDomain: "https://real-state-api-mocha.vercel.app/",
+  authDomain: "real-state-api-mocha.vercel.app",
   projectId: "mern-state-40a4f",
   storageBucket: "mern-state-40a4f.appspot.com",
   messagingSenderId: "798614973604",
