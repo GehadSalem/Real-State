3510efede496cc124021e43af0b3e99637258784
Gehad Salem Abd El-shafy <35219807+GehadSalem@users.noreply.github.com>
2024-09-06 23:46:19 +0300

    edit

--- src/firebase.js
+++ src/firebase.js
@@ -9,8 +9,8 @@ import { BASEURL } from "./data/dataApi.jsx";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_API_KEY,
-  authDomain: `${BASEURL}`,
+  apiKey: "AIzaSyBMEA9_mJV155UQLVOTvW9l959KVJK5bug",
+  authDomain: `real-state-api-mocha.vercel.app`,
   projectId: "mern-state-40a4f",
   storageBucket: "mern-state-40a4f.appspot.com",
   messagingSenderId: "798614973604",
