5dced0a4495d0e33881ac836a1aa212d590c49ef
Gehad Salem Abd El-shafy <35219807+GehadSalem@users.noreply.github.com>
2024-09-11 22:29:06 +0300

    edit

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ export default function App() {
     <BrowserRouter>
       <Header />
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/(.*)" element={<Home />} />
         <Route path="/sign-in" element={<SignIn />} />
         <Route path="/sign-up" element={<SignUp />} />
         <Route path="/about" element={<About />} />
